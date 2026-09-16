7ba2497250e3b61da3d82c5dffdbfaa03be69e1d
khatrirohan305-ui <khatrirohan305@gmail.com>
2026-09-16 14:07:41 +0900

    Update Project

--- config/settings.py
+++ config/settings.py
@@ -122,4 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ['192.168.1.98', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
+
+
